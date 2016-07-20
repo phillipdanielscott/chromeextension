841194447dac603b5084c6aae1cdc5855dcfc0fc
phillipdanielscott <phillipd20@yahoo.com>
2016-07-20 17:46:27 -0400

    first commit

---
+++ function.js
@@ -0,0 +1,25 @@
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    message.innerText = request.source;
+  }
+});
+
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
+window.onload = onWindowLoad;
+
+
+
+document.body.innerHTML = document.body.innerHTML.replace("Trump", "The man with the golden hair");
---
+++ popup.js
@@ -0,0 +1,1 @@
+document.body.innerHTML = document.body.innerHTML.replace(/Trump/gi, "Man with the golden hair");
